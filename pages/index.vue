c3d3f64424c7c4bd9b7278291f0f02ba8bf7e397
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2022-12-14 23:32:10 +0700

    fix: add ring's link back

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,20 @@ import * as data from "../data.ts";
       <div
         class="top-0 left-0 mx-auto flex flex-row items-center gap-4 md:mx-0"
       >
-        <img
-          src="@/assets/img/ring-thin.svg"
-          alt="Creatorsgarten Ring"
-          class="h-[32px] w-[32px]"
-        />
-        <img
-          src="@/assets/img/webring.svg"
-          alt="Webring.in.th Ring"
-          class="h-[32px] w-[32px]"
-        />
+        <a href="https://creatorsgarten.org/ring" title="Creatorsgarten Ring">
+          <img
+            src="@/assets/img/ring-thin.svg"
+            alt="Creatorsgarten Ring"
+            class="h-[32px] w-[32px]"
+          />
+        </a>
+        <a href="https://webring.wonderful.software#tinarskii.com" title="วงแหวนเว็บ">
+          <img
+            src="@/assets/img/webring.svg"
+            alt="Webring.in.th Ring"
+            class="h-[32px] w-[32px]"
+          />
+        </a>
       </div>
 
       <!-- Logo -->
