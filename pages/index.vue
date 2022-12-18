d4e6cd442b8b937ba935510b97930a7b1eee5c43
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2022-12-18 16:59:59 +0700

    feat: add home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template>Hello</template>
+<template>
+  <div class="flex w-full cursor-pointer flex-row">
+    <img
+      src="/banner.jpg"
+      alt="Tin at National Science Museum"
+      class="h-[65vh] w-full rounded-xl object-cover"
+    />
+  </div>
+</template>
