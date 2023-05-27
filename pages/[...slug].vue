6ea6433dad487c7e600f692835e22be48aab4837
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2023-05-27 12:24:34 +0700

    Ddd

---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup>
+import { links } from "~/data/link";
+
+const route = useRoute();
+const slug = route.params.slug
+
+if (links[slug]) {
+  navigateTo(links[slug], { external: true });
+}
+</script>
+
+<template>
+  <slot />
+</template>
